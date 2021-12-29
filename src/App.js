d4f4581ef371c690f36b2b1c00cdcd6104507050
yussems <yussems@gmail.com>
2021-12-29 21:59:14 +0300

    added mouseenterevent

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [left, setLeft] = useState(null);
+  const [top, settop] = useState(null);
+
+  const handleEnter = () => {
+    setLeft(Math.floor(Math.random() * 600) + 1);
+    settop(Math.floor(Math.random() * 300) + 1);
+  };
+
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>Yıl sonu anketi</h1>
+      </div>
+
+      <div className="content">
+        <div className="App">
+          <h3>Maaşınıza zam istiyor musunuz?</h3>
+          <div className="button">
+            <button className="btnFirst">Evet</button>
+
+            <button onMouseEnter={handleEnter} className="btnSecond" style={{ left: left, top: top }}>
+              Hayır
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
